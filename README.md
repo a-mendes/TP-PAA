57744dcc75c474cb7765d5760dfb26ecb8825205
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2025-03-11 14:55:29 -0300

    feat: geração de instancias

---
+++ README.md
@@ -0,0 +1,38 @@
+# TP-PAA
+
+Este é um projeto Python que serve como um template para construir aplicações. Abaixo estão as instruções sobre como configurar, executar e testar a aplicação.
+
+## Estrutura do Projeto
+
+```
+tp-paa
+├── src
+│   ├── __init__.py
+│   └── main.py
+├── requirements.txt
+├── setup.py
+└── README.md
+```
+
+## Instalação
+
+Para instalar as dependências necessárias, execute o seguinte comando:
+
+```
+pip install -r requirements.txt
+```
+
+## Executando a Aplicação
+
+Para executar a aplicação, use o seguinte comando:
+
+```
+python -m src.main
+```
+## Geração de instâncias
+
+Para executar a geração das instâncias, use o seguinte comando:
+
+```
+python -m src.instance_generator
+```
---
+++ src/instance_generator.py
@@ -0,0 +1,63 @@
+import os
+import random
+
+EXPERIMENT_1_DIR = 'instances/experiment_1'
+EXPERIMENT_2_DIR = 'instances/experiment_2'
+
+WEIGHT_MAX = 30
+VALUE_MAX = 100
+
+def generate_experiment_1():
+    n = 100
+    for i in range(1, 21):
+        W = random.randint(0, 10 * n)  
+        items = [(random.randint(1, WEIGHT_MAX), random.randint(1, VALUE_MAX)) for _ in range(n)] 
+
+        filename = f'{EXPERIMENT_1_DIR}/{i}.txt'
+        
+        with open(filename, 'w') as f:
+            f.write(f"{W}\n")
+            for wi, vi in items:
+                f.write(f"{wi}\t{vi}\n")
+        
+        print(f"Generated {filename}")
+        
+        n *= 2
+
+def generate_experiment_2():
+    n = 400
+    W = 100
+    for i in range(1, 21):
+        items = [(random.randint(1, WEIGHT_MAX), random.randint(1, VALUE_MAX)) for _ in range(n)]  
+
+        filename = f'{EXPERIMENT_2_DIR}/{i}.txt'
+        
+        with open(filename, 'w') as f:
+            f.write(f"{W}\n")
+            for wi, vi in items:
+                f.write(f"{wi}\t{vi}\n")
+
+        print(f"Generated {filename}")
+        
+        W *= 2
+
+def generate_instances():
+    print("Generating instances...")
+
+    if not os.path.exists('instances'):
+        os.makedirs('instances')
+    if not os.path.exists(EXPERIMENT_1_DIR):
+        os.makedirs(EXPERIMENT_1_DIR)
+    if not os.path.exists(EXPERIMENT_2_DIR):
+        os.makedirs(EXPERIMENT_2_DIR)
+
+    print("Generating experiment 1 instances...")
+    generate_experiment_1()
+
+    print("Generating experiment 2 instances...")
+    generate_experiment_2()
+
+    print("Successfully generated instances")
+
+if __name__ == "__main__":
+    generate_instances()

5f6c8301af146a88aa4140a863f0f39eb0b2cd7a
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2025-03-17 15:24:11 -0300

    refactor: naming pattern

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 import os
 from consts import EXPERIMENT_1_DIR, EXPERIMENT_2_DIR
 
-def ler_dados(experimento, instancia):
-    if experimento not in [1, 2] or not (1 <= instancia <= 20):
-        raise ValueError("Experimento deve ser 1 ou 2 e instancia deve estar entre 1 e 20")
+def read_instance(experiment, instance):
+    if experiment not in [1, 2] or not (1 <= instance <= 20):
+        raise ValueError("Experimento deve ser 1 ou 2 e instance deve estar entre 1 e 20")
 
-    base_path = EXPERIMENT_1_DIR if experimento == 1 else EXPERIMENT_2_DIR
-    file_path = os.path.join(base_path, f'instancia{instancia}.txt')
+    base_path = EXPERIMENT_1_DIR if experiment == 1 else EXPERIMENT_2_DIR
+    file_path = os.path.join(base_path, f'{instance}.txt')
 
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Arquivo {file_path} não encontrado")
@@ -14,11 +14,11 @@ def ler_dados(experimento, instancia):
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
-    peso_mochila = int(lines[0].strip())
-    matriz_itens = []
+    backpack_weight = int(lines[0].strip())
+    itens = []
 
     for line in lines[1:]:
-        peso, valor = map(int, line.strip().split())
-        matriz_itens.append([peso, valor])
+        weight, value = map(int, line.strip().split())
+        itens.append([weight, value])
 
-    return peso_mochila, matriz_itens
+    return backpack_weight, itens

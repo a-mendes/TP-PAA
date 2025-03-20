2bd2edc32746adf2a94a37349e9d51722a3af5b7
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2025-03-19 21:20:45 -0300

    feat: create menu flow

--- src/main.py
+++ src/main.py
@@ -1,22 +1,42 @@
 import algorithms.dynamic_programming as dp
+import algorithms.branch_and_bound as bd
 import utils as ut
 
 def experiment_dynamic_programming(experiment):
-
-    instance_start = ut.last_instance_executed(experiment, ut.c.DYANMIC_PROGRAMMING)
+    instance_start = ut.last_instance_executed(experiment, ut.c.DYNAMIC_PROGRAMMING)
     if(instance_start == 0):
-        ut.delete_logs(experiment, ut.c.DYANMIC_PROGRAMMING);
+        ut.delete_logs(experiment, ut.c.DYNAMIC_PROGRAMMING)
 
-    for i in range(instance_start + 1, 21):
+    # for i in range(instance_start + 1, 21):
+    for i in range(instance_start + 1, 10):
         print(f"Instance {i}")
-        dp.backpack_dynamic_programing(experiment, i)
+        dp.execute(experiment, i)
+
+def experiment_branch_and_bound(experiment):
+    instance_start = ut.last_instance_executed(experiment, ut.c.BRANCH_AND_BOUND)
+    if(instance_start == 0):
+        ut.delete_logs(experiment, ut.c.BRANCH_AND_BOUND)
+
+    # for i in range(instance_start + 1, 21):
+    for i in range(instance_start + 1, 10):
+        print(f"Instance {i}")
+        bd.execute(experiment, i)
+
+def experiment_backtracking(experiment):
+    pass
+
+def start_experiments(experiment):
+    experiment_dynamic_programming(experiment)
+    experiment_branch_and_bound(experiment)
+    experiment_backtracking(experiment)
+
 
 def main():
-    #print("Experiment 1 - Dynamic Programming")
-    #experiment_dynamic_programming(1)
+    print("Experiment 1")
+    start_experiments(1)
 
-    print("Experiment 2 - Dynamic Programming")
-    experiment_dynamic_programming(2)
+    print("Experiment 2")
+    start_experiments(1)
 
 if __name__ == "__main__":
     main()
